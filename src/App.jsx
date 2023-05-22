a6fddc7fe4f115012ddd052320173c46920fd8dc
dszwarc <dszwarc93@gmail.com>
2023-05-21 22:49:54 -0400

    added styling for bio. linked semantic react ui

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ function App() {
   return (
     <>
     <BioCard />
-    
+    <footer>
+    </footer>
     </>
   )
 }
--- src/components/BioCard.jsx
+++ src/components/BioCard.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import 
+import './BioCard.css'
 
 export default function BioCard(){
     return(
-        <h1>this is my bio card</h1>
+            <div id='name-title-div'>
+                <h1 id='name'>Dan Szwarc</h1>
+                <h3 id='my-title'>Software Engineer</h3>
+                <a href='../utils/DS_Resume.pdf' download>Download Resume</a>
+                <p id='my-bio'>My name is Dan and I am a mechanical engineer turned software engineer. I have chosen to challenge myself and change careers from one demanding field to the next. I bring my love of problem-solving and obsessive need to know how things work from my previous career into the realm of software engineering in the hopes of push myself even further.</p>
+            </div>
     )
 }
